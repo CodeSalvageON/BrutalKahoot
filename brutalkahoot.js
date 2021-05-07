9a711f818f79af36fb872386fa61bd25dee1ee57
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-05-07 14:57:33 -0400

    Create brutalkahoot.js

---
+++ brutalkahoot.js
@@ -0,0 +1,14 @@
+// Brutal Kahoot 
+// Begin
+
+// Variables 
+
+const number_of_rounds = document.querySelector(".top-bar__QuestionNumber-sc-186o9v8-2 hAQMGb");
+
+// Change text in Kahoot to Brutal Kahoot text
+
+function changeText () {
+  number_of_rounds.innerText = number_of_rounds.innerText + " ROUNDS USED IN CLIP";
+}
+
+changeText();
